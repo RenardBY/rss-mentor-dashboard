2dc37e5cffd2efc3a513f58ac247aa2a780f0ff0
Andrei Poleschuk <renardby@mail.ru>
2019-03-04 18:20:20 +0200

    feat: create dashboard

---
+++ precache-manifest.b38474478e6769e3b7a07447a1759f14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8854a7ac60fae1f5e9bd",
+    "url": "/renardby/static/js/runtime~main.8854a7ac.js"
+  },
+  {
+    "revision": "e6f8a764b6241a325eb2",
+    "url": "/renardby/static/js/main.e6f8a764.chunk.js"
+  },
+  {
+    "revision": "7489798e1f84925f0aad",
+    "url": "/renardby/static/js/1.7489798e.chunk.js"
+  },
+  {
+    "revision": "e6f8a764b6241a325eb2",
+    "url": "/renardby/static/css/main.8dca7bf4.chunk.css"
+  },
+  {
+    "revision": "7489798e1f84925f0aad",
+    "url": "/renardby/static/css/1.8aaa1776.chunk.css"
+  },
+  {
+    "revision": "2fd15e40852429adc5fb93586a6c35eb",
+    "url": "/renardby/index.html"
+  }
+];
